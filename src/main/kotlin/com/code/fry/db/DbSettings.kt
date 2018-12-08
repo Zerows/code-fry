aaf2d048ff1a3880a4dce96538df58cdd68a46da
shrikanthkr <shrikanth_kr@yahoo.com>
2018-12-08 20:28:57 +0530

    add db connection pool for runner

--- src/main/kotlin/com/code/fry/db/DbSettings.kt
+++ src/main/kotlin/com/code/fry/db/DbSettings.kt
@@ -1,10 +1,18 @@
 package com.code.fry.db
 
+import org.apache.commons.dbcp2.BasicDataSource
 import org.jetbrains.exposed.sql.Database
 
 object DbSettings {
     val DB by lazy {
-        Database.connect("jdbc:postgresql://localhost:5433/codefry", driver = "org.postgresql.Driver",
-                user = "postgres", password = "example")
+        val ds = BasicDataSource()
+        ds.url = "jdbc:postgresql://localhost:5433/codefry"
+        ds.username = "postgres"
+        ds.password = "example"
+        ds.maxOpenPreparedStatements = 2
+        ds.maxIdle = 2
+        ds.minIdle = 1
+        ds.maxTotal = 2
+        Database.connect(ds)
     }
 }
