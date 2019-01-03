6aa5c60b455c4c1469c70cdcbb932bff521f4dd7
shrikanthkr <shrikanth_kr@yahoo.com>
2019-01-03 15:45:49 +0530

    revert to pads

--- src/main/kotlin/com/code/fry/dao/Result.kt
+++ src/main/kotlin/com/code/fry/dao/Result.kt
@@ -7,7 +7,7 @@ import org.jetbrains.exposed.dao.IntEntityClass
 class Result(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<Result>(Results)
 
-    var runner_id by Results.runner_id
+    var submission by Pad referencedOn Results.pad
     var output by Results.output
     var error by Results.error
     var status by Results.status
--- src/main/kotlin/com/code/fry/dao/Results.kt
+++ src/main/kotlin/com/code/fry/dao/Results.kt
@@ -3,7 +3,7 @@ package com.code.fry.dao
 import org.jetbrains.exposed.dao.IntIdTable
 
 object Results : IntIdTable() {
-    val runner_id = integer("runner_id")
+    val pad = reference("pad_id", Pads)
     val output = text("output").nullable()
     val error = text("error").nullable()
     val status = enumeration("status", ResultStatus::class) // will create integer column
