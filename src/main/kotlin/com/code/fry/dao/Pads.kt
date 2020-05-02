160a3fce44111b8a0eaeef5f8bf5e2932ca25395
shrikanthkr <shrikanth_kr@yahoo.com>
2020-05-02 18:09:40 +0530

    remove dependent results column

--- src/main/kotlin/com/code/fry/dao/Pads.kt
+++ src/main/kotlin/com/code/fry/dao/Pads.kt
@@ -6,6 +6,6 @@ object Pads : IntIdTable() {
     val language = text("language")
     val filename = text("filename")
     val content = text("content")
-    val createdAt = Results.date("created_at")
-    val updatedAt = Results.date("updated_at")
+    val createdAt = date("created_at")
+    val updatedAt = date("updated_at")
 }
