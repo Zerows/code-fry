d2ad09d61c39d5022ab1439a26e563241d1f974c
shrikanthkr <shrikanth_kr@yahoo.com>
2020-05-03 15:50:55 +0530

    add a dummy line for new file

--- src/main/kotlin/com/code/fry/languages/PythonRunner.kt
+++ src/main/kotlin/com/code/fry/languages/PythonRunner.kt
@@ -2,10 +2,12 @@ package com.code.fry.languages
 
 import com.code.fry.command.Command
 import com.code.fry.command.Resource
+import java.io.File
 
 class PythonRunner(resource: Resource) : Runner(resource) {
 
     override fun run(): Boolean {
+        val file = File(resource.file)
         return Command.execute("python", getFileAbsPath())
 
     }
